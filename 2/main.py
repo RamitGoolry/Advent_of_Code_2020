e7fe2bf068d174339c662cd1b0a1067a74180bd0
Ramit Goolry <rgoolry@ucsd.edu>
2021-01-05 12:44:44 +0530

    Completed Q2P1

---
+++ 2/main.py
@@ -0,0 +1,22 @@
+# FILE = 'example.txt'
+FILE = 'test.txt'
+
+def is_valid(line):
+    condition, password = [x.strip() for x in line.split(':')]
+
+    # Parsing Condition
+    range, char = condition.split()
+    lower, upper = [int(n) + 1 for n in range.split('-')]
+
+    count = len(filter(lambda x : x == char, password))
+
+    return lower <= count <= upper
+
+def main():
+    with open(FILE) as file:
+        lst = file.read().split('\n')[:-1]
+
+    print(sum(map(is_valid, lst)))
+
+if __name__ == '__main__':
+    main()
